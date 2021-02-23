f8c4a455b2626479da7674480573a1fec756054b
Karan Gandhi <kag354@rutgers.edu>
2021-02-23 13:53:51 -0500

    startup file

--- CLASSapplePIE/ViewController.swift
+++ CLASSapplePIE/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
+{
+
+    
+    @IBOutlet weak var imageOutput: UIImageView!
+    
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    
+    @IBOutlet var letterButton: [UIButton]!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    
+    var listOfWords = ["box", "cheese", "butter"]
+    
+    var lives = 0
+    var wins = 0
+    var losses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        newRound()
+    }
+    
+    
+    func newRound(){
+        
+        
+        let word = listOfWords.removeFirst()
+        
+        
+        updateUI()
     }
 
+    
+    func updateUI(){
+        
+        
+    }
+    
 
 }
 
